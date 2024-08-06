ce1acc8ec0853a86f17f42f4dabf18927320fca4
amarshyna <anastasiya.marshyna@hiqo-solutions.com>
2024-08-06 18:05:35 +0400

    created form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:task_1/layout_3.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +13,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Layout with IntrinsicHeight and IntrinsicWidth'),
-          ),
-          body: const Layout3()
-
-          // TASK: 7636
-          // const SingleChildScrollView(
-          //   padding: EdgeInsets.all(16.0),
-          //   child: Center(child: Column(
-          //   children: [Layout1(), SizedBox(height: 30), Layout2()])),
-          // ),
-          ),
-    );
+        title: 'Localizations Sample App',
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Fill the form'),
+            ),
+            body: Center(
+                child: Column(children: [
+              Localizations.override(
+                context: context,
+                locale: const Locale('es'),
+                // Using a Builder to get the correct BuildContext.
+                // Alternatively, you can create a new widget and Localizations.override
+                // will pass the updated BuildContext to the new widget.
+                child: Builder(
+                  builder: (context) {
+                    // A toy example for an internationalized Material widget.
+                    return CalendarDatePicker(
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime(2100),
+                      onDateChanged: (value) {},
+                    );
+                  },
+                ),
+              ),
+              ContactForm()
+            ]))));
   }
 }
